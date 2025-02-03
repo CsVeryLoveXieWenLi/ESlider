dc8bedf2e4c602dc143f2a46bf73f3707d661cbe
CsVeryLoveXieWenLi <MCBW666@163.com>
2025-02-03 21:51:28 +0800

    Init -> 初始化项目

---
+++ src/Main.cpp
@@ -0,0 +1,84 @@
+﻿/**
+ * @Author: CsVeryLoveXieWenLi
+ * @LastEditTime: 2025-02-03 21:46:21
+ * @Description: 主程序
+ * @Copyright (c) 2024 by CsVeryLoveXieWenLi, All Rights Reserved.
+ */
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+/**
+ * 易语言：识别滑块
+ */
+int ESliderOcr(unsigned char* bg_bytes, int blength, unsigned char* gap_bytes, int glength) {
+    if (bg_bytes == nullptr || gap_bytes == nullptr || blength == 0 || glength == 0) return 0;
+
+    // 从内存中解码图片，忽略透明度通道
+    // 若忽略透明度通道，全透明处则变为黑色背景
+    cv::Mat bg_image  = cv::imdecode(cv::Mat(1, blength, CV_8UC1, bg_bytes), cv::IMREAD_COLOR);
+    cv::Mat gap_image = cv::imdecode(cv::Mat(1, glength, CV_8UC1, gap_bytes), cv::IMREAD_COLOR);
+
+    if (bg_image.empty() || gap_image.empty()) return 0;
+
+
+    // 对大图进行高斯模糊，减少边缘信息
+    // 然后进行边缘检测，获得更清晰的轮廓信息
+    cv::Mat bg_image_canny;
+    cv::GaussianBlur(bg_image, bg_image, cv::Size(7, 7), 0.0);
+
+    cv::Canny(bg_image, bg_image_canny, 190.0, 300.0);
+
+
+    // 对小图进行扩充，防止检测不到边缘
+    // 对小图进行二值化，凸显图中的缺口
+    // 然后进行边缘检测，获得更清晰的轮廓信息
+    cv::Mat gap_image_canny;
+    cv::copyMakeBorder(gap_image, gap_image, 10, 10, 10, 10, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0));
+    cv::cvtColor(gap_image, gap_image, cv::COLOR_BGR2GRAY);
+    cv::threshold(gap_image, gap_image, 0.0, 255.0, cv::THRESH_BINARY);
+
+    cv::Canny(gap_image, gap_image_canny, 190.0, 300.0);
+
+
+    // 对小图进行轮廓检测，提取图中的缺口
+    // 寻找面积最大的轮廓，便是缺口
+
+    // 警告，若图中缺口边缘有纯黑像素，则可能存在问题
+    // 可使用别的方法定位图中缺口，这里就用这个，懒得写别的
+    std::vector<std::vector<cv::Point>> gap_image_contours;
+    cv::findContours(gap_image_canny, gap_image_contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    if (gap_image_contours.empty()) return 0;
+
+    double max_area  = 0.0;
+    int    max_index = 0;
+
+    for (int index = 0, num = gap_image_contours.size(); index < num; index++) {
+        auto& contour = gap_image_contours[index];
+
+        double area = cv::contourArea(contour);
+        if (area > max_area) {
+            max_area  = area;
+            max_index = index;
+        }
+    }
+
+    cv::Mat crop_image_canny = gap_image_canny(cv::boundingRect(gap_image_contours[max_index]));
+
+
+    // 进行模板匹配，查找最优结果
+    cv::Mat   result;
+    cv::Point point;
+    cv::matchTemplate(bg_image_canny, crop_image_canny, result, cv::TM_CCORR);
+    cv::minMaxLoc(result, nullptr, nullptr, nullptr, &point);
+
+    return point.x;
+}
+
+
+/**
+ * 易语言：设置线程数
+ */
+void ESliderThread(int num) { cv::setNumThreads(num); }
---
+++ xmake.lua
@@ -0,0 +1,121 @@
+﻿--[[
+Author: CsVeryLoveXieWenLi
+LastEditTime: 2024-11-18 12:14:32
+Description: ESlider
+Copyright (c) 2024 by CsVeryLoveXieWenLi, All Rights Reserved. 
+--]]
+
+-- update -> xmake project -k compile_commands
+
+
+-- 运行库
+add_cxflags("/MT")
+
+
+-- 模式
+add_rules("mode.release")
+
+
+-- 头文件路径
+add_includedirs("D:\\Libs\\StaticMT\\opencv-4.10.0\\x86\\include")
+
+
+-- 库文件夹路径
+add_linkdirs("D:\\Libs\\StaticMT\\opencv-4.10.0\\x86\\lib")
+
+
+-- Opencv
+add_links("libjpeg-turbo.lib")
+add_links("libopenjp2.lib")
+add_links("libpng.lib")
+add_links("libwebp.lib")
+add_links("opencv_core4100.lib")
+add_links("opencv_imgcodecs4100.lib")
+add_links("opencv_imgproc4100.lib")
+add_links("zlib.lib")
+
+
+-- 语言版本
+set_languages("clatest", "c++20")
+
+
+-- 任务配置
+target("ESlider")
+    set_kind("shared")
+
+    add_files("src/*.cpp")
+    add_files("src/export.def")
+
+    set_symbols("hidden")
+    set_strip("all")
+
+
+--
+-- If you want to known more usage about xmake, please see https://xmake.io
+--
+-- ## FAQ
+--
+-- You can enter the project directory firstly before building project.
+--
+--   $ cd projectdir
+--
+-- 1. How to build project?
+--
+--   $ xmake
+--
+-- 2. How to configure project?
+--
+--   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
+--
+-- 3. Where is the build output directory?
+--
+--   The default output directory is `./build` and you can configure the output directory.
+--
+--   $ xmake f -o outputdir
+--   $ xmake
+--
+-- 4. How to run and debug target after building project?
+--
+--   $ xmake run [targetname]
+--   $ xmake run -d [targetname]
+--
+-- 5. How to install target to the system directory or other output directory?
+--
+--   $ xmake install
+--   $ xmake install -o installdir
+--
+-- 6. Add some frequently-used compilation flags in xmake.lua
+--
+-- @code
+--    -- add debug and release modes
+--    add_rules("mode.debug", "mode.release")
+--
+--    -- add macro definition
+--    add_defines("NDEBUG", "_GNU_SOURCE=1")
+--
+--    -- set warning all as error
+--    set_warnings("all", "error")
+--
+--    -- set language: c99, c++11
+--    set_languages("c99", "c++11")
+--
+--    -- set optimization: none, faster, fastest, smallest
+--    set_optimize("fastest")
+--
+--    -- add include search directories
+--    add_includedirs("/usr/include", "/usr/local/include")
+--
+--    -- add link libraries and search directories
+--    add_links("tbox")
+--    add_linkdirs("/usr/local/lib", "/usr/lib")
+--
+--    -- add system link libraries
+--    add_syslinks("z", "pthread")
+--
+--    -- add compilation and link flags
+--    add_cxflags("-stdnolib", "-fno-strict-aliasing")
+--    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
+--
+-- @endcode
+--
+

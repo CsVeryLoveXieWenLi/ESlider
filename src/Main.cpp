b73e9b99a01325594eda713b9416d9f08a6262c0
CsVeryLoveXieWenLi <MCBW666@163.com>
2025-02-06 19:04:42 +0800

    Update -> 优化阈值、优化寻找小图里面缺口的算法、优化示例

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,6 @@
 ﻿/**
  * @Author: CsVeryLoveXieWenLi
- * @LastEditTime: 2025-02-03 21:46:21
+ * @LastEditTime: 2025-02-06 19:02:22
  * @Description: 主程序
  * @Copyright (c) 2024 by CsVeryLoveXieWenLi, All Rights Reserved.
  */
@@ -13,66 +13,61 @@
  * 易语言：识别滑块
  */
 int ESliderOcr(unsigned char* bg_bytes, int blength, unsigned char* gap_bytes, int glength) {
-    if (bg_bytes == nullptr || gap_bytes == nullptr || blength == 0 || glength == 0) return 0;
+    // 非法数据
+    if (bg_bytes == nullptr || gap_bytes == nullptr || blength <= 0 || glength <= 0) return 0;
 
-    // 从内存中解码图片，忽略透明度通道
-    // 若忽略透明度通道，全透明处则变为黑色背景
+    // 解码，若小图有透明通道，直接转化全透明为黑色背景
     cv::Mat bg_image  = cv::imdecode(cv::Mat(1, blength, CV_8UC1, bg_bytes), cv::IMREAD_COLOR);
     cv::Mat gap_image = cv::imdecode(cv::Mat(1, glength, CV_8UC1, gap_bytes), cv::IMREAD_COLOR);
 
+    // 解码图片失败
     if (bg_image.empty() || gap_image.empty()) return 0;
 
 
-    // 对大图进行高斯模糊，减少边缘信息
-    // 然后进行边缘检测，获得更清晰的轮廓信息
-    cv::Mat bg_image_canny;
-    cv::GaussianBlur(bg_image, bg_image, cv::Size(7, 7), 0.0);
+    // 处理大图和小图
+    cv::Mat gap_image_canny, bg_image_canny;
 
-    cv::Canny(bg_image, bg_image_canny, 190.0, 300.0);
+    cv::GaussianBlur(bg_image, bg_image, cv::Size(3, 3), 0.0);
+    cv::Canny(bg_image, bg_image_canny, 120.0, 220.0);
 
-
-    // 对小图进行扩充，防止检测不到边缘
-    // 对小图进行二值化，凸显图中的缺口
-    // 然后进行边缘检测，获得更清晰的轮廓信息
-    cv::Mat gap_image_canny;
-    cv::copyMakeBorder(gap_image, gap_image, 10, 10, 10, 10, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0));
-    cv::cvtColor(gap_image, gap_image, cv::COLOR_BGR2GRAY);
-    cv::threshold(gap_image, gap_image, 0.0, 255.0, cv::THRESH_BINARY);
-
-    cv::Canny(gap_image, gap_image_canny, 190.0, 300.0);
+    cv::GaussianBlur(gap_image, gap_image, cv::Size(9, 9), 0.0);
+    cv::Canny(gap_image, gap_image_canny, 140.0, 240.0);
 
 
-    // 对小图进行轮廓检测，提取图中的缺口
-    // 寻找面积最大的轮廓，便是缺口
+    // 寻找缺口
+    int    w = gap_image_canny.cols, h = gap_image_canny.rows, step = gap_image_canny.step;
+    int    x1 = gap_image_canny.cols, y1 = gap_image_canny.rows, x2 = 0, y2 = 0;
+    uchar* data = gap_image_canny.data;
 
-    // 警告，若图中缺口边缘有纯黑像素，则可能存在问题
-    // 可使用别的方法定位图中缺口，这里就用这个，懒得写别的
-    std::vector<std::vector<cv::Point>> gap_image_contours;
-    cv::findContours(gap_image_canny, gap_image_contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    for (int y = 0; y < h; y++) {
+        int ptr = y * step;
 
-    if (gap_image_contours.empty()) return 0;
+        for (int x = 0; x < w; x++) {
+            uchar pixel = data[ptr + x];
+            if (pixel == 0) continue;
 
-    double max_area  = 0.0;
-    int    max_index = 0;
+            if (x < x1) x1 = x;
+            if (y < y1) y1 = y;
 
-    for (int index = 0, num = gap_image_contours.size(); index < num; index++) {
-        auto& contour = gap_image_contours[index];
-
-        double area = cv::contourArea(contour);
-        if (area > max_area) {
-            max_area  = area;
-            max_index = index;
+            if (x > x2) x2 = x;
+            if (y > y2) y2 = y;
         }
     }
 
-    cv::Mat crop_image_canny = gap_image_canny(cv::boundingRect(gap_image_contours[max_index]));
+    // 裁剪得到缺口
+    cv::Mat crop_image_canny = gap_image_canny(cv::Rect(x1, y1, x2 - x1 + 1, y2 - y1 + 1));
 
 
-    // 进行模板匹配，查找最优结果
+    // 模板匹配
     cv::Mat   result;
     cv::Point point;
-    cv::matchTemplate(bg_image_canny, crop_image_canny, result, cv::TM_CCORR);
+
+    // cv::imwrite("crop_image_canny.jpg", crop_image_canny);
+    // cv::imwrite("bg_image_canny.jpg", bg_image_canny);
+
+    cv::matchTemplate(bg_image_canny, crop_image_canny, result, cv::TM_CCOEFF);
     cv::minMaxLoc(result, nullptr, nullptr, nullptr, &point);
+
 
     return point.x;
 }
